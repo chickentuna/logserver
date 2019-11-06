efb2305da8c575b53c05242197fb9da383fd3537
Julien <julien@codingame.com>
2019-11-06 21:21:09 +0100

    made working prototype

---
+++ client.js
@@ -0,0 +1,26 @@
+var socket = io();
+
+
+function append(line) {
+  const [_, timestamp, severity, account, message] = line.split(/(\[.*\]) (\[.*\]) (?:<(.*)> )?(.*)/)
+  let el
+  if (line === '') {
+    el = $(`<p class="line"><br></p>`)
+  } else {
+    el = $(`<p class="line">`).text(line)
+  }
+  if (severity && severity.includes('ERROR')) {
+    el.addClass('error')
+  }
+  $('#lines').append(el)
+}
+
+socket.on('line', function(line) {
+  append(line)
+})
+
+socket.on('history', function(history) {
+  history.forEach(line => {
+    append(line)
+  })
+})
--- server.js
+++ server.js
@@ -4,9 +4,17 @@ var app = express()
 var server = require('http').Server(app)
 var io = require('socket.io').listen(server)
 var Tail = require('tail').Tail;
- 
-tail = new Tail("logs.txt", {fromBeginning: true});
- 
+const argv = require('yargs')
+.option('file', {
+  alias: 'f',
+  describe: 'the log file to read from'
+})
+.argv
+
+const history = []
+
+tail = new Tail(argv.file, {fromBeginning: true});
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'index.html'))
 })
@@ -16,6 +24,8 @@ app.use(express.static('.'))
 
 io.on('connection', function (socket) {
   console.log('a user connected')
+  socket.emit('history', history)
+
   socket.on('disconnect', function () {
     console.log('user disconnected')
   })
@@ -27,6 +37,7 @@ server.listen(8081, function () {
 
 
 tail.on("line", function(data) {
+  history.push(data)
   io.emit('line', data)
 });
  
