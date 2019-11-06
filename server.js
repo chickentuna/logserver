eacb2a6c15531482851941f0d704f20d5c0ce6a0
Julien <julien.poulton@codingame.com>
2019-11-06 14:05:23 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,37 @@
+var express = require('express')
+var path = require('path')
+var app = express()
+var server = require('http').Server(app)
+var io = require('socket.io').listen(server)
+var Tail = require('tail').Tail;
+ 
+tail = new Tail("logs.txt");
+ 
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'index.html'))
+})
+
+app.use(express.static('.'))
+// app.use(express.static(path.join(__dirname, 'public')))
+
+io.on('connection', function (socket) {
+  console.log('a user connected')
+  socket.on('disconnect', function () {
+    console.log('user disconnected')
+  })
+})
+
+server.listen(8081, function () {
+  console.log(`Listening on ${server.address().port}`)
+})
+
+
+tail.on("line", function(data) {
+  io.emit('line', data)
+});
+ 
+tail.on("error", function(error) {
+  console.log('ERROR: ', error);
+});
+
+
