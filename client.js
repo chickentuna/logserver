ab3c7e0b7007f6850a773948ee2e1aeefd514763
Julien Poulton <julien.poulton@codingame.com>
2019-11-25 14:12:55 +0100

    link conversion

--- client.js
+++ client.js
@@ -1,21 +1,25 @@
 var socket = io();
 
-
-// const allLineEls = []
-
+//[Mon 25 13:53] [LOG] <BoldLibrarian_9666> https://playr.gg/contest/ECngjLZ
 function append(line) {
-  const [_, timestamp, severity, account, message] = line.split(/(\[.*\]) (\[.*\]) (?:<(.*)> )?(.*)/)
+  // const [_, timestamp, severity, account, message] = line.split(/(\[.*\]) (\[.*\]) (?:<(.*)> )?(.*)/)
   let el
   if (line === '') {
     el = $(`<p class="line"><br></p>`)
   } else {
-    el = $(`<p class="line">`).text(line.replace(/(https:\/\/[^ ]+)/, '<a href="$1">$1</a>'))
+    let linkMatch = /(https:\/\/[^ ]+)/.exec(line)
+    if (linkMatch) {
+      el = $(`<p class="line">`).text(line)
+      const escaped = el.innerHTML
+      el.html(el.html().replace(/(https:\/\/[^ ]+)/, `<a href="$1">$1</a>`))
+    } else {
+      el = $(`<p class="line">`).text(line)
+    }    
   }
   if (severity && severity.includes('ERROR')) {
     el.addClass('error')
   }
   $('#lines').append(el)
-  // allLineEls.push(el)
 }
 
 socket.on('line', function(line) {
